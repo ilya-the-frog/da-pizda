2b9dc526f7130e14678bfa90e8628309422f1516
Shantaramka <kontroler.im@gmail.com>
2020-06-18 18:23:52 +0300

    adding all code

--- main.go
+++ main.go
@@ -1,60 +1,67 @@
 package main
 
 import (
-  "github.com/go-telegram-bot-api/telegram-bot-api"
-  "log"
-  "io/ioutil"
-  "time"
-  "fmt"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"io/ioutil"
+	"log"
+	"time"
 )
 
 func main() {
-  //достанем токен из файла
-  token, err := ioutil.ReadFile("token.txt")
-   if err != nil {
-     log.Printf("File reading error", err)
-     return
-     }
-  // подключаемся к боту с помощью токена
-  bot, err := tgbotapi.NewBotAPI(string(token))
+	//достанем токен из файла
+	token, err := ioutil.ReadFile("token.txt")
+	if err != nil {
+		log.Printf("File reading error", err)
+		return
+	}
+
+	// подключаемся к боту с помощью токена
+	bot, err := tgbotapi.NewBotAPI(string(token))
 	if err != nil {
 		log.Panic(err)
 	}
 
-  bot.Debug = true
+	bot.Debug = false
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	// инициализируем канал, куда будут прилетать обновления от API
 	var ucfg tgbotapi.UpdateConfig = tgbotapi.NewUpdate(0)
 	ucfg.Timeout = 60
 	upd, _ := bot.GetUpdatesChan(ucfg)
-  time.Sleep(time.Millisecond * 500)
-  upd.Clear()
+	time.Sleep(time.Millisecond * 500)
+	upd.Clear()
 	// читаем обновления из канала
 	for {
 		select {
 		case update := <-upd:
-      //проверяем, от канала или от пользователя
-      if update.ChannelPost == nil {
-        fmt.Println(update)
-        UserName := update.Message.From.UserName   // Пользователь, который написал боту
-			  ChatID := update.Message.Chat.ID   // ID чата/диалога.
-			  Text := update.Message.Text // Текст сообщения
-        Photo := update.Message.Photo //фотки
-			  log.Printf("[%s] %d %s", UserName, ChatID, Text)
-        reply := Text
-        // чекаю фотки или нет
-        if update.Message.Caption == "" {
-          reply = Text + "\n \n" + "Автор поста: @" + UserName
-        } else {
-          reply = update.Message.Caption + "\n \n" + "Автор поста: @" + UserName
-        }
-			  // Ответим пользователю его же отредаченным сообщением
+			//проверяем, от канала или от пользователя
+			if update.ChannelPost == nil {
+				//fmt.Println(update)
+				UserName := update.Message.From.UserName // Пользователь, который написал боту
+				ChatID := update.Message.Chat.ID         // ID чата/диалога.
+				Text := update.Message.Text              // Текст сообщения
+				// Photo := update.Message.Photo            //фотки
+				log.Printf("[%s] %d %s", UserName, ChatID, Text)
+				var reply = "" // чекаю текст
 
-			  // Созадаем сообщение
-			  msg := tgbotapi.NewMessage(-1001196308797, reply)
-			  bot.Send(msg) // и отправляем его
-    }
+				if update.Message.Text == "да" || update.Message.Text == "Да" {
+					reply = "пизда"
+				}
+
+				if update.Message.Text == "пизда" || update.Message.Text == "Пизда" {
+					reply = "да"
+				}
+
+				if update.Message.Text == "нет" || update.Message.Text == "Нет" {
+					reply = "пидора ответ"
+				}
+
+				if reply != "" {
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
+					msg.BaseChat.ReplyToMessageID = update.Message.MessageID //добавляем реплай
+					bot.Send(msg)                                            // и отправляем его
+				}
+			}
 		}
 	}
 }
