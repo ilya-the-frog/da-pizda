5050dbeb1ad81dd7b5588a557bf10883c902c157
semyonf1l1pp0v <syomchik13@gmail.com>
2025-09-11 23:41:50 +0300

    Minor codestyle fixes, sirupsen/logrus for logs

--- dapizda.go
+++ dapizda.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"github.com/go-telegram-bot-api/telegram-bot-api"
 	"io/ioutil"
-	"log"
 	"strings"
 	"time"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	log "github.com/sirupsen/logrus"
 )
 
 var answers = map[string]string{
@@ -20,47 +21,47 @@ var answers = map[string]string{
 	"ytn":      "пидора ответ",
 	"здрасьте": "забор покрасьте",
 	"300":      "отсоси у тракториста",
-	"триста":   "отсоси у тракториста"}
+	"триста":   "отсоси у тракториста",
+}
 
 func main() {
 	//достанем токен из файла
 	token, err := ioutil.ReadFile("token.txt")
 	if err != nil {
-		log.Printf("File reading error", err)
+		log.WithError(err).Error("File reading error")
 		return
 	}
 
 	// подключаемся к боту с помощью токена
 	bot, err := tgbotapi.NewBotAPI(string(token))
 	if err != nil {
-		log.Fatalf("Error connecting to the bot: %v", err)
+		log.WithError(err).Fatal("Error connecting to the bot")
 	}
 
 	bot.Debug = false
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+	log.WithField("account", bot.Self.UserName).Print("Authorized on account")
 
 	// инициализируем канал, куда будут прилетать обновления от API
 	var ucfg tgbotapi.UpdateConfig = tgbotapi.NewUpdate(0)
 	ucfg.Timeout = 60
 	upd, err := bot.GetUpdatesChan(ucfg)
 	if err != nil {
-		log.Fatalf("Error getting updates channel: %v", err)
+		log.WithError(err).Fatal("Error getting updates channel")
 	}
 	time.Sleep(time.Millisecond * 500)
 	upd.Clear()
 	// читаем обновления из канала
 	for {
-		select {
-		case update := <-upd:
+		if update, ok := <-upd; ok {
 			//проверяем, от канала или от пользователя
 			if update.ChannelPost == nil && update.EditedMessage == nil {
-				if reply := answers[strings.ToLower(update.Message.Text)]; reply != "" {
+				if reply, ok := answers[strings.ToLower(update.Message.Text)]; ok {
 					msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
 					msg.BaseChat.ReplyToMessageID = update.Message.MessageID //добавляем реплай
-					log.Printf("Sending %s", reply)
+					log.WithField("reply", reply).Print("Sending reply")
 					_, err := bot.Send(msg)
 					if err != nil {
-						log.Fatalf("Error sending message: %v", err)
+						log.WithError(err).Fatal("Error sending message")
 					}
 				}
 			}
